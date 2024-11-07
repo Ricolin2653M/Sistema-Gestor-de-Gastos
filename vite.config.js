a62d93215cebde875bc133033db4068323398bd2
liliana De Jesús Martínez <2021371047@uteq.edu.mx>
2024-11-07 10:11:25 -0600

    manifest listo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    viteStaticCopy({
+      targets:[
+        
+        {
+          src:'sw.js',
+          dest: ''
+        },
+        {
+          src:'manifest.json',
+          dest: ''
+        },
+        
+       
+       
+        {
+          src:'public',
+          dest: ''
+        },
+        {
+          src:'icons',
+          dest: 'assets/img'
+        },
+
+      ],
+      }),
+      react(),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames:'assets/index-001.js',
+      },
+    },
+  },
+});
