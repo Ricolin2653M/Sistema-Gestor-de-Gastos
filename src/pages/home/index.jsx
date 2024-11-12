13aaaf2829bb6e07588a8d4ed4e573df6114e08a
liliana De Jesús Martínez <2021371047@uteq.edu.mx>
2024-11-11 20:50:11 -0600

    lo puedes checar segun yo ya esta

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,14 +6,10 @@ import "./home.css";
 const Home = () => {
   return (
     <div className="home-container">
-      
-      <div className="table-section" style={{ marginTop: '30px' }}>
-     
+      <div className="table-section">
         <div className="table-wrapper">
           <Deposits />
         </div>
-      
-
         <div className="table-wrapper">
           <Expenses />
         </div>
